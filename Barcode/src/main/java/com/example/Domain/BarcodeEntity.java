3c916d15bc4189013fe744b2e5be57310c8bf00c
김주영 <jyk102999@dsm.hs.kr>
2022-06-03 22:15:47 +0900

    🎫 :: Barcode

--- Barcode/src/main/java/com/example/Domain/BarcodeEntity.java
+++ Barcode/src/main/java/com/example/Domain/BarcodeEntity.java
@@ -1,4 +1,12 @@
 package com.example.Domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class BarcodeEntity {
+    
 }
